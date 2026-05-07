60becc8943bc82eab11597c92699569f9f5ddc86
mikealvarez9999 <mikealvarez9999@gmail.com>
2026-05-07 22:35:53 +0600

    Fixed Google Analytics integration

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import './globals.css';
+import Script from 'next/script';
 
 import icon192 from '../assets/icons/icon-192.png';
 import icon512 from '../assets/icons/icon-512.png';
@@ -58,7 +59,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`} strategy="afterInteractive" />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);} 
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', { page_path: window.location.pathname });
+          `}
+        </Script>
+        {children}
+      </body>
     </html>
   );
 }
