6bf2810a3089b9bf74667633ddf3c2c0d869cec5
mikealvarez9999 <mikealvarez9999@gmail.com>
2026-09-03 00:20:16 +0600

    Replace header text with theme-aware logos

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -3,12 +3,17 @@
 import AuthBox from './AuthBox';
 import ThemeToggle from './ThemeToggle';
 import nestAuthLogo from '../../assets/icons/IMG_8338.PNG';
+const logoDark = '/logos/logo-white.png';
+const logoLight = '/logos/logo-black.png';
 
 export default function Header() {
   return (
     <header id="appHeader">
       <div className="brand">
-        <h1>IUB Course Planner</h1>
+        <div className="logo-wrapper">
+          <img className="logo-dark" src={logoDark} alt="IUB Course Planner" />
+          <img className="logo-light" src={logoLight} alt="IUB Course Planner" />
+        </div>
         <div className="creator-line">
           <div className="sub">Created with curiosity by Raiyan Bin Rais</div>
           <img className="nest-auth-logo nest-auth-logo-mobile" src={nestAuthLogo.src} alt="IUB NEST auth logo" />
